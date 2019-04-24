5dc93b56cfa034a6397a7567f28ccff55c134905
renton18 <gakituka18@gmail.com>
2019-04-24 22:39:51 +0900

    commit

---
+++ SakuraEditorSnipet/SQL抽出_シングルクオーテーション.js
@@ -0,0 +1,20 @@
+var cnt = GetLineCount(0);
+var textAll = "";
+for (var i = 1; i <= cnt; i++) {
+    // 行の文字列を取得
+    var str = GetLineStr(i).replace(/\r\n/,"").replace(/\n/,"");
+    str = trim();
+    // コード削除
+    str = str.replace(/^.*?\'/,"");		// 行頭から最初のダブルクォーテーションまでを削除
+	str = str.replace(/\'.*?$/, "");	// 行末から最初のダブルクォーテーションまでを削除
+
+    str = str.replace(/ +$/,"");		// 行頭の空白削除
+    str = str.replace(/^ /,"");			// 行末の空白削除    
+    
+    //str = str.replace(/.:.*?[ ]/g," '' ");		// :を''に入れ替え
+    //str = str.replace(/.:.*?$/," '' ");			// :を''に入れ替え
+    
+    textAll += str + "\n";
+}
+SelectAll(0);
+InsText(textAll);
