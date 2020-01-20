be28600aed81b280dd325612fbfd5230a51996ee
renton18 <gakituka18@gmail.com>
2020-01-20 22:35:21 +0900

    commit

---
+++ MODULE/SQLSERVER_BALK.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+
+namespace IDNO新旧変換
+{
+    class SQLSERVER_BALK
+    {
+        public string tableName { get; set; } = "";
+        public string serverName { get; set; } = "";
+        public string id { get; set; } = "";
+        public string pass { get; set; } = "";
+        public string importFilePath { get; set; } = "";
+        public string exportFilePath { get; set; } = "";
+        public string formatFilePath { get; set; } = "";
+
+        #region インポート
+        public void Import()
+        {
+            var proc = new Process();
+            proc.StartInfo.FileName = "BCP";
+            proc.StartInfo.Arguments = tableName + @" IN """ + importFilePath + @""" -f """ + formatFilePath + @""" -S " + serverName + " -U " + id + " -P " + pass;
+            proc.Start();
+
+        }
+        #endregion
+
+        #region エクスポート
+        public void Export()
+        {
+            var proc = new Process();
+            proc.StartInfo.FileName = "BCP";
+            proc.StartInfo.Arguments = tableName + @" OUT """ + exportFilePath + @""" -f """ + formatFilePath + @""" -S " + serverName + " -U " + id + " -P " + pass;
+            proc.Start();
+
+        }
+        #endregion
+    }
+}
